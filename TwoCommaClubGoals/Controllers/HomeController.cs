d04bc21c0feabb94ab0aec0e950edea01bb8cecc
Eduardo Davila <DAVILAE1@my.erau.edu>
2019-04-08 14:24:10 -0700

    Add project files.

---
+++ TwoCommaClubGoals/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TwoCommaClubGoals.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Who are we?";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "If you would like to reach out to the 2CommaClub team \n feel free to do so using one of the methods below.";
+
+            return View();
+        }
+        public ViewResult Register()
+        {
+            return View();
+        }
+        public ViewResult UnderConstruction()
+        {
+            return View();
+        }
+    }
+}
---
+++ TwoCommaClubGoals/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TwoCommaClubGoals.MvcApplication" Language="C#" %>
